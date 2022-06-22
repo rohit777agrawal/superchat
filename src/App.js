9b60aca9a48de5c550e95db1f74ff4376265ab19
rohit777agrawal <rohit777agrawal@gmail.com>
2022-06-21 21:43:12 -0700

    API changed

--- src/App.js
+++ src/App.js
@@ -12,13 +12,21 @@ import {useCollectionData} from 'react-firebase-hooks/firestore'
 
 // This help to identify the project
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID
+
+  apiKey: "AIzaSyAei2UwCB9boBGgthxcL6hg7LOAO45lau8",
+  authDomain: "superchat-76c6f.firebaseapp.com",
+  projectId: "superchat-76c6f",
+  storageBucket: "superchat-76c6f.appspot.com",
+  messagingSenderId: "198007717454",
+  appId: "1:198007717454:web:887e0acf63c8d8b66a0d1f"
+
 })
 
 //Reference to Firebase SDK'S for global variables
