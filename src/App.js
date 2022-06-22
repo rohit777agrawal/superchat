3f15d88a00324319369cfa7ed3b9f6a2f2f42b11
rohit777agrawal <rohit777agrawal@gmail.com>
2022-06-22 01:32:44 -0700

    sounds effect and deploy!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import './App.css'
+import sound from './assets/sound.mp3'
 
 //These are imports of firebase SDK
 import firebase from 'firebase/compat/app'
@@ -12,20 +13,13 @@ import {useCollectionData} from 'react-firebase-hooks/firestore'
 
 // This help to identify the project
 firebase.initializeApp({
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID
-
-  apiKey: "AIzaSyAei2UwCB9boBGgthxcL6hg7LOAO45lau8",
-  authDomain: "superchat-76c6f.firebaseapp.com",
-  projectId: "superchat-76c6f",
-  storageBucket: "superchat-76c6f.appspot.com",
-  messagingSenderId: "198007717454",
-  appId: "1:198007717454:web:887e0acf63c8d8b66a0d1f"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 
 })
 
@@ -70,10 +64,14 @@ function SignOut(){
 
 function ChatRoom(){
   const messageRef = firestore.collection('messages');  //This reference to the collection
-  const query = messageRef.orderBy('createdAt').limit(25); //This reference to the timestamp it was created.
+  const query = messageRef.orderBy('createdAt'); //This reference to the timestamp it was created.
   const [messages] = useCollectionData(query, { idField: 'id' });
-
   const dummy = useRef();
+
+  let audio = new Audio(sound);
+  const start = () => {
+  audio.play()
+  }
 
   const [formValue, setFormValue] = useState('');
 
@@ -87,18 +85,20 @@ function ChatRoom(){
         photoURL
     });
     setFormValue('');
+    start();
     dummy.current.scrollIntoView({behavior:'smooth'});
   }
 
   return(
     <>
+      
       <main>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
         <div ref={dummy}></div>
       </main>
 
       {/* "sendMessage is going to write to the database" */}
-      <form onSubmit={sendMessage}>
+      <form onSubmit={sendMessage} >
         <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
         <button type='Submit'>Send</button>
       </form>
